0448fe32c8f834811c6a76c87625140b91a47b05
Ruan Oliveira <ruan.amaral@compasso.com.br>
2025-03-20 22:31:51 -0300

    feat: adding reset css

--- FrontEnd/src/main.tsx
+++ FrontEnd/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./main.css";
 import ContactList from "./pages/ContactList.tsx";
 
 createRoot(document.getElementById("root")!).render(
