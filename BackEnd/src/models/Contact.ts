ab2423a53ebb1e66c1b729d5777e39c3a19ae37f
Ruan Oliveira <ruan.amaral@compasso.com.br>
2025-03-21 13:44:20 -0300

    feat (back-end): creating contact model for db

---
+++ BackEnd/src/models/Contact.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const contactSchema = new mongoose.Schema({
+  id: { type: String },
+  name: { type: String, required: true },
+  cpf: { type: String, required: true },
+  email: { type: String, required: true },
+  phone: { type: String, required: true },
+  havePhoto: { type: Boolean, default: false },
+});
+
+const contacts = mongoose.model("contacts", contactSchema);
+
+export default contacts;
