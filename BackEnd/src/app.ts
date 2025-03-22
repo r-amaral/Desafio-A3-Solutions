11774ae92383eb656d4c6cdcc4c8ad12d5364cba
Ruan Oliveira <ruan.amaral@compasso.com.br>
2025-03-21 23:59:43 -0300

    feat (back-end): add cors

--- BackEnd/src/app.ts
+++ BackEnd/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import db from "./config/dbConnect";
 import routes from "./routes/index";
+import cors from "cors";
 
 db.on("error", console.log.bind(console, "Connection error"));
 db.once("open", () => {
@@ -9,6 +10,7 @@ db.once("open", () => {
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 routes(app);
 
 export default app;
